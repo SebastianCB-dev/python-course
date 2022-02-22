e1f44c025b1b12782b8826d1271ebdeabdc0c7f8
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2022-02-22 07:29:17 -0500

    Init Commit

---
+++ Leccion01/main.py
@@ -0,0 +1,18 @@
+miVariable = "Hello from Python 😊"
+print(miVariable)
+print(miVariable)
+print(miVariable)
+
+miVariable = 0.001
+print(miVariable)
+
+x = 10
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
+# memory id
+print(id(x))
+
+
