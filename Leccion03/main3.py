3a2599ec25a751503a50ae5f0e393a01f7942473
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2022-03-13 20:08:36 -0500

    Feat: Added Leccion 03

---
+++ Leccion03/main3.py
@@ -0,0 +1,11 @@
+condicion = True
+
+#if condicion:
+    #print('Condición verdadera')
+#else:
+    #print('Condición falsa')
+
+# Ternario
+
+print('Condición verdadera') if condicion else print('Condicion falsa')
+
