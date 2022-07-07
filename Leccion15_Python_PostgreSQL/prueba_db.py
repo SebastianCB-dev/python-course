08acd1133a6b1ac568e51739ceb5d5b9d13a0655
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-07-07 15:06:10 -0500

    Feat: Added psycopg2 connection

---
+++ Leccion15_Python_PostgreSQL/prueba_db.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+connection = psycopg2.connect(user="postgres",
+                              password="root",
+                              host="localhost", # O bien host="127.0.0.1",
+                              port="5432",
+                              database="test_db");
+
+# If the connection is successful, it will print the connection object
+# else it will print the error message
+# print( connection ) 
+
+try:
+  with connection as conn:
+    with conn.cursor() as cursor:
+      sentence = "SELECT * FROM people;"
+      cursor.execute(sentence)
+      registros = cursor.fetchall()
+      print(registros)
+except(Exception) as e:
+  print("Error: unable to fetch data -", e)
+finally:
+  connection.close()
+
+
+  
