4958751880467659eebf532150dfc7ccedc5e4e5
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-07-17 18:04:45 -0500

    Feat: Added Basic CRUD

---
+++ Leccion15_Python_PostgreSQL/transactions/transacciones.py
@@ -0,0 +1,31 @@
+import psycopg2 as pg
+
+conn = pg.connect(user="postgres",
+                  password="root",
+                  host="localhost",
+                  port="5432",
+                  database="python_postgres")
+
+# Con el with el commit y el rollback se ejecutan automaticamente
+# Commit es cuando se ejecuta correctamente
+# Rollback es cuando se ejecuta mal y se cancela la transaccion
+try:
+  # Que no se guarden los cambios automaticamente
+  conn.autocommit = False # Si se coloca en True se omite el conn.commit(), no es buena practica
+
+  cursor = conn.cursor()
+  sentence = 'INSERT INTO people(name, last_name, email) VALUES (%s, %s, %s)'
+  values = ('Maria', 'Esparza', 'mesparza@gmail.com')
+  cursor.execute(sentence, values)
+  sentence = 'UPDATE people SET last_name = %s WHERE id = %s'
+  values = ('Gonzalez', 1)
+  cursor.execute(sentence, values)
+
+  conn.commit() # Se guardan los datos
+  print('Transaction finished')
+except Exception as e:
+  conn.rollback()
+  print('Error doing rollback:', e)
+finally:
+  conn.close()
+  print("Connection closed")
